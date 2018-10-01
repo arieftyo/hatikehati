b2d6fe749e42831f259a8b1303cfaea042ff86b2
arieftyo <36990643+arieftyo@users.noreply.github.com>
2018-10-01 09:07:52 +0700

    tugas2

--- app.py
+++ app.py
@@ -60,8 +60,9 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-    c=text
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\n'+text=c))
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\n'+event.message.text))
+
 
     
 import os
